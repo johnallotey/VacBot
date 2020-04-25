ab956d51eb31c8ae1b0acd8ee79a0e921d3198d6
johnallotey <52253990+johnallotey@users.noreply.github.com>
2020-04-25 18:35:50 -0400

    Add files via upload

---
+++ TravelBot/travel.js
@@ -0,0 +1,90 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const {Amadeus} = require('amadeus');
+const axios = require('axios');
+
+
+const amadeus = new Amadeus({
+	clientId: 'iRMst5wsBZaRmUZ8xo6pZVqZd0xeSW6V',
+	clientSecret: 'TYA0ZkQgvXDKnOHt'
+});
+
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ axios.get('https://test.api.amadeus.com/v1/security/oauth2/token\?grant_type=client_credentials&client_id={iRMst5wsBZaRmUZ8xo6pZVqZd0xeSW6V}&client_secret={TYA0ZkQgvXDKnOHt}').then(response => {
+  console.log(response.data.url);
+  console.log(response.data.explanation);
+})
+.catch(error => {
+  console.log(error);
+});
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  
+  
+ function ama(){
+	amadeus.shopping.flightOffersSearch.get({
+  	originLocationCode: 'SYD',
+  	destinationLocationCode: 'BKK',
+   	departureDate: '2020-08-01',
+   adults: '2'
+	}).then(function(response){
+	  console.log(response.data);
+	}).catch(function(responseError){
+ 	 console.log(responseError.code);
+	});
+ 	 }
+  // // Uncomment and edit to make your own intent handler
+  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://assistant.google.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
+
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
+  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
---
+++ TravelBot/travel.py
@@ -0,0 +1,36 @@
+from amadeus import Client, ResponseError
+import logging
+import flask
+import requests
+import json
+from flask import request, jsonify
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+logger = logging.getLogger('your_logger')
+logger.setLevel(logging.DEBUG)
+
+amadeus = Client(
+    client_id='','',
+    log_level='debug'
+)
+
+@app.route('/destination/<origin>', methods=['GET'])
+def get_flight(origin):
+# Flight Inspiration Search
+	r = amadeus.shopping.flight_destinations.get(origin='MAD')
+	#json_data = json.loads(r)
+	#print(r.result)
+	return jsonify(r.result)
+
+#@app.route('/snap_info', methods=['GET'])
+#def home():
+   # return jsonify(get_info())
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify("John")
+app.run() 
